990cf4a86373a565ce1f9093e9bd39276a61bc52
linchengweiii <linchengweiii@gmail.com>
2024-07-10 00:37:05 +0800

    feat: use workaround to preload images

--- src/App.vue
+++ src/App.vue
@@ -7,47 +7,42 @@
     @next-modal="nextModal"
   >
     <img v-if="'image' in item" :src="item.image" alt="Paris" />
-    <p v-else v-for="(line, lineIndex) in item.content.split('\n')" :key="lineIndex">{{ line }}</p>
   </NextDialog>
+  <img v-show="false" v-for="(src, index) in images" :key="index" :src="src" alt="Image" />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import NextDialog from './components/NextDialog.vue'
 import paris from './assets/paris.jpg'
+import firstdiving from './assets/firstdiving.jpg'
 
-interface ImageContent {
+interface Content {
   title: string
-  image: string
+  image?: string
 }
 
-interface TextContent {
-  title: string
-  content: string
-}
+const images = [paris, firstdiving]; // Preload images
 
-const contents: (ImageContent | TextContent)[] = [
+const contents: Content[] = [
   {
-    title: '生日快樂！',
-    content: ''
+    title: '生日快樂！'
   },
   {
-    title: '這可是我嘔心瀝血做的生日禮物，你一定要喜歡喔！',
-    content: ''
+    title: '這可是我嘔心瀝血做的生日禮物，你一定要喜歡喔！'
   },
   {
-    title: '我們一起經歷了好多事情',
-    content: '',
+    title: '我們一起經歷了好多事情'
   },
   {
     title: '一起在法國旅行',
     image: paris
   },
   {
-    title: '一起在法國旅行2',
-    content: 'haha'
+    title: '一起去小琉球潛水',
+    image: firstdiving
   }
-]
+];
 
 const indexOpeningDialog = ref(0)
 
